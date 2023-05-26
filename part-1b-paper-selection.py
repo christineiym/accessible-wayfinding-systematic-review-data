45887fa3884d8e4bc3200a026d051e086d76ec0d
Christine Mendoza <christine.mendoza@unc.edu>
2023-05-26 02:44:26 -0400

    rework paper selection file organization

---
+++ part-1b-paper-selection.py
@@ -0,0 +1,7 @@
+"""Utilities for running the paper selection process.
+
+Simply change paths for the input and output files for each stage of the paper selection process,
+then run on the command line (after cd'ing to this directory) with the following command:
+
+python -m part-1b-paper-selection
+"""
