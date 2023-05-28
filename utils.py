79120a572733d60bdc2732942ea3c311448047f6
Christine Mendoza <christine.mendoza@unc.edu>
2023-05-28 06:57:11 -0400

    write out steps

--- utils.py
+++ utils.py
@@ -5,3 +5,9 @@ then run on the command line (after cd'ing to this directory) with the following
 
 python -m part-2-paper-selection
 """
+
+# Steps:
+# Stage 1: For each paper in all papers, input 0 or 1 for each category in Exclusion Stage 1.
+# Stage 2: For each paper for which all criteria are 0 in Stage 1, input 0 or 1 for each category in Exclusion Stage 2.
+# Duplicates: Mark duplicates in the table after identifying relationships.
+# For each paper that passes Stage 2 AND is not a duplicate, enter criteria for each dimension table.
